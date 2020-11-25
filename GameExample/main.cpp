8577e9edae6e73f775e830f96c92320d729eb1e5
Denis Migranov <d.migranov@g.nsu.ru>
2020-11-25 18:15:12 +0700

    интересный эксперимент с большим радиусом

--- GameExample/main.cpp
+++ GameExample/main.cpp
@@ -23,6 +23,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPWSTR, _
     Texture * sunTexture = game.CreateTexture(L"8k_sun.dds");
 
     
+    //auto earth = new SphericalSphere(0.999999, 35, 35, earthTexture, SphericalRotationYW(3 * XM_PI / 2));
     auto earth = new SphericalSphere(0.92f, 35, 35, earthTexture, SphericalRotationYW(3 * XM_PI / 2));
     game.AddMesh(earth);
     
