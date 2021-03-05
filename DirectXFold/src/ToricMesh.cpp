a70424b85a16a8913056063c1f1909b3c6098758
Denis Migranov <d.migranov@g.nsu.ru>
2021-03-05 20:15:51 +0700

    конструкторы реализованы

--- DirectXFold/src/ToricMesh.cpp
+++ DirectXFold/src/ToricMesh.cpp
@@ -1,14 +1,11 @@
 #include "pch.h"
 #include "ToricMesh.h"
 
-ToricMesh::ToricMesh()
-{
-}
+ToricMesh::ToricMesh() : Mesh()
+{}
 
-ToricMesh::ToricMesh(int nv, VertexPosColor* vertices, int ni, WORD* indices)
-{
-}
+ToricMesh::ToricMesh(int nv, VertexPosColor* vertices, int ni, WORD* indices) : Mesh(nv, vertices, ni, indices)
+{ }
 
-ToricMesh::ToricMesh(int nv, VertexPosColor* vertices, int ni, WORD* indices, DirectX::XMMATRIX world)
-{
-}
+ToricMesh::ToricMesh(int nv, VertexPosColor* vertices, int ni, WORD* indices, DirectX::XMMATRIX world) : Mesh(nv, vertices, ni, indices, world)
+{ }
