c711e0b5181ce151c7d8315fb263032fa2bd760f
Denis Migranov <d.migranov@g.nsu.ru>
2021-03-06 18:42:37 +0700

    Toric Cube начат

---
+++ DirectXFold/inc/ToricCube.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "ToricMesh.h"
+
+//в новой версии Mesh должен быть общий для евклидова случая и торического,
+//разница в системе рендеринга!
+class ToricCube :
+    public ToricMesh
+{
+};
+
